fd1ff61760e1ae76de894fabd04f682423391dfd
Noémi Salaün <noemi.salaun@gmail.com>
2014-05-21 16:52:02 +0200

    Improve interpolation

--- app/public/cube.js
+++ app/public/cube.js
@@ -40,9 +40,18 @@
 
   Cube.prototype.interpolate = function(states, time) {
     createjs.Tween.removeTweens(this);
-    var threshold = time / 200;
+    var threshold = Math.min(1, time / 200);
     var scale = states.scale < threshold ? 0 : states.scale;
-    createjs.Tween.get(this.states).to({x: states.x, y: states.y, scale: scale}, time, createjs.Ease.linear);
+    if (scale < 1) {
+      createjs.Tween.get(this.states).to({scale: scale}, time, createjs.Ease.linear);
+      if (states.teleport.moved) {
+        this.states.x = states.x;
+        this.states.y = states.y;
+      }
+    } else {
+      createjs.Tween.get(this.states).to({x: states.x, y: states.y, scale: scale}, time, createjs.Ease.linear);
+    }
+
   };
 
   window.Cube = Cube;
